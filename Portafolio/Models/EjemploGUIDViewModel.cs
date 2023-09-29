7980e42a26ff0fb1e61ea17b4ac599f15405b57d
Omar Sanchez <ing_omarglez@hotmail.com>
2023-09-29 15:54:59 -0600

    Creando mi portafolio C# , .Net, Blazor, Bootstrap

---
+++ Portafolio/Models/EjemploGUIDViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Portafolio.Models
+{
+	public class EjemploGUIDViewModel
+	{
+		public Guid Transitorio { get; set; }
+		public Guid Delimitado { get; set; }
+		public Guid Unico { get; set; }
+
+	}
+
+}
